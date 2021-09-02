6d408b5b23d69b2c4bee45e5e6436f73a96a177f
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-09-02 12:30:23 +0100

    Basic highlight working

--- www/cgi-bin/theme_search.py
+++ www/cgi-bin/theme_search.py
@@ -2,6 +2,9 @@
 import json
 import sys
 from pathlib import Path
+from whoosh import index
+from whoosh.qparser import MultifieldParser
+
 import cgi
 import cgitb
 cgitb.enable()
@@ -23,6 +26,40 @@ def dispatch_action(action,form):
 
     if action == "mugshots":
         list_group_leaders()
+
+    elif action == "search":
+        run_search(form["term"].value)
+
+
+
+def run_search(term):
+    index_folder = Path(__file__).parent.parent.parent / "index"
+    ix = index.open_dir(index_folder)
+
+    qp = MultifieldParser(["title","abstract"],schema=ix.schema)
+
+    query = qp.parse(term)
+
+    with ix.searcher() as searcher:
+        results = searcher.search(query, limit=None)
+
+        json_data = {}
+        for result in results:
+
+            if not result["person"] in json_data:
+                json_data[result["person"]] = []
+
+            snippets = result.highlights("title").split("...")
+            snippets.extend(result.highlights("abstract").split("..."))
+
+            snippets = [x for x in snippets if x]
+            
+            json_data[result["person"]].append({
+                "title":result["title"],
+                "snippets":snippets
+            })
+
+    send_json(json_data)
 
 
 def list_group_leaders():
--- www/js/theme_search.js
+++ www/js/theme_search.js
@@ -3,7 +3,7 @@ const backend = "/cgi-bin/theme_search.py"
 $( document ).ready(function() {
     console.log("Running")
     populate_mugshots(undefined)
-    $("#searchbox").keypress(update_search)
+    $("#searchbox").keyup(update_search)
 
 })
 
@@ -44,12 +44,66 @@ function populate_mugshots(data) {
         image_data = data[i]
         console.log("Adding "+image_data["name"])
 
-        div.append(`<img src="${image_data["url"]}" title="${image_data["name"]}" height="100px">`)
+        // We need a lowercase version of name with no spaces to act
+        // as an id tag
+        name_id = image_data["name"].toLowerCase()
+        name_id = name_id.replaceAll(" ","_")
+
+        div.append(`<div class="mugwrapper d-inline-block position-relative" id="${name_id}"><span class="badge position-absolute top-100 start-50 text-light p-1 bg-danger">100</span><img class="mugshot" src="${image_data["url"]}" title="${image_data["name"]}" height="75px"></div>`)
 
     }
 
 }
 
-function update_search() {
+function update_search() { 
+    search_text = $("#searchbox").val()
+
+    if (search_text.length < 3) {
+        // TODO: Clear previous results
+        return
+    }
+    console.log("Searching with "+search_text)
+
+    $.ajax(
+        {
+            url: backend,
+            data: {
+                action: "search",
+                term: search_text
+            },
+            success: function(data) {
+                add_search_results(data)
+            },
+            error: function(message) {
+                console.log("Failed to run search "+message)
+            }
+        }
+    )
+
+function add_search_results(data) {
+    // Data is a dict with names as keys and a list
+    // as value with a dict with title and snippets
+    // in it.
+
+    // Reset any previous results
+    $(".mugshot").addClass("faded")
+    $("span").hide()
+
+    // First go through the keys to grey out any 
+    // group leaders with no hits.
+
+    for (let [name,hits] of Object.entries(data)) {
+
+        name_id = name.toLowerCase()
+        name_id = name_id.replaceAll(" ","_")
+
+        console.log("Enabling "+name_id)
+        $("#"+name_id).find("img").removeClass("faded")
+
+    }
+
+
+}
+
 
 }

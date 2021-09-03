a793d47a09b5f1fdd8912d6fb84f49ea7a221583
Simon Andrews <simon.andrews@babraham.ac.uk>
2021-09-03 08:38:52 +0100

    Expand and filter key terms.
    Add primary documents and key terms results.

--- scripts/main_themes.py
+++ scripts/main_themes.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 from pathlib import Path
 from whoosh import index
-from whoosh.qparser import QueryParser
+from whoosh.qparser import QueryParser, MultifieldParser
+import re
 
 def main():
     index_folder = Path("../index/")
@@ -32,18 +33,84 @@ def main():
                         if term[0] in key_terms and key_terms[term[0]] > term[1]:
                             continue
                         key_terms[term[0]] = term[1]
+
                 except ValueError:
                     print("Problem with getting terms from abstract")
 
                 key_terms = {k: v for k, v in sorted(key_terms.items(), reverse=True, key=lambda item: item[1])}
 
-                for i,term in enumerate(key_terms.keys()):
-                    if i == 20:
+                # Get expanded versions of the most important terms
+                expanded_terms = expand_key_terms(key_terms, ix)
+
+                # We might have duplicates in the expanded terms so we'll record what 
+                # we've used
+                used_terms = []
+
+                for term in key_terms.keys():
+                    eterm = expanded_terms[term]
+                    if eterm.isdigit():
+                        continue
+                    if eterm in used_terms:
+                        continue
+                    if len(eterm)<3:
+                        continue
+
+                    used_terms.append(eterm)
+                    print("\t".join([str(x) for x in [leader,len(used_terms),round(key_terms[term],2),eterm]]), file=kt)
+
+                    if len(used_terms) == 20:
                         break
 
-                    print("\t".join([str(x) for x in [leader,i+1,round(key_terms[term],2),term]]), file=kt)
-
                 print("",file=kt)
+
+def expand_key_terms(short_terms, ix):
+    # Some of the key terms are word stems rather than complete
+    # words so we want to be able to put actual words in instead
+    # Here we'll do a search with each key term and see what comes
+    # back.
+
+    expanded_terms = {}
+
+    qp = MultifieldParser(["title","abstract"],schema=ix.schema)
+
+    for short_term in short_terms:
+        # We need to add the * because some short terms don't actually
+        # produce any hits when entered without a suitable suffix
+        query = qp.parse(short_term+"*")
+
+        snippet_hits = {}
+
+        with ix.searcher() as searcher:
+            results = searcher.search(query, limit=None)
+
+            for result in results:
+                snippets = result.highlights("title").split("...")
+                snippets.extend(result.highlights("abstract").split("..."))
+                snippets = [x for x in snippets if x]
+
+                for snippet in snippets:
+                    hit = re.search(">(.*?)</b>", snippet)
+                    if hit is not None:
+                        snippet_hit = hit.groups()[0].lower()
+
+                        if not snippet_hit in snippet_hits:
+                            snippet_hits[snippet_hit] = 1
+                        
+                        else:
+                            snippet_hits[snippet_hit] += 1
+
+        # Find the most common term for the snippet
+        if not snippet_hits:
+            breakpoint()
+
+        best_hit = sorted(list(snippet_hits.keys()), key=lambda x: snippet_hits[x], reverse=True)[0]
+
+        expanded_terms[short_term] = best_hit
+
+    return expanded_terms
+
+
+
 
 
 
